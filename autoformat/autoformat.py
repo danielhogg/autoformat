5134894f55f54bd45d2dff24df15f00f260db840
Daniel Hogg <daniel.hogg@mcgill.ca>
2019-03-07 15:08:21 -0500

    working rough draft

--- autoformat/autoformat.py
+++ autoformat/autoformat.py
@@ -1,6 +1,7 @@
 import datetime
 
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import numpy as np
 
 
@@ -10,5 +11,51 @@ def scale(start_date, end_date):
         raise ValueError("start_date must be before end_date.")
 
     dynamic_range = end_date - start_date
+    total_seconds = dynamic_range.total_seconds()
+    total_hours = total_seconds / 3600
+
+    fig, ax = plt.subplots()
+
+    majorLoc = mdates.AutoDateLocator()
+    minorFormat = None
+    minorLoc = None
+    majorFormat = None
+    xlabel = None
 
     
+    if total_hours < 1:
+        pass
+    elif total_hours < 6:
+        majorFormat = mdates.DateFormatter("%H:%M")
+    elif total_hours < 12:
+        majorFormat = mdates.DateFormatter("%Hh")
+    elif total_hours <= 24 * 7:
+        majorFormat = mdates.DateFormatter("%b %d")
+        minorFormat = mdates.DateFormatter("%H")
+        minorLoc = majorLoc
+        majorLoc = mdates.DayLocator()
+        fig.autofmt_xdate()
+    elif total_hours < 24 * 7 * 4:
+        majorFormat = mdates.DateFormatter("%b %d")
+        majorLoc = mdates.DayLocator()
+        fig.autofmt_xdate()
+    else:
+        pass
+        # Currently out of scope
+
+
+    if majorLoc is not None:
+        ax.xaxis.set_major_locator(majorLoc)
+
+    if majorFormat is not None:
+        ax.xaxis.set_major_formatter(majorFormat)
+
+    if minorLoc is not None:
+        ax.xaxis.set_minor_locator(minorLoc)
+
+    if minorFormat is not None:
+        ax.xaxis.set_minor_formatter(minorFormat)
+
+    if xlabel is not None:
+        plt.xlabel(xlabel)
+
--- tests/test_autoformat.py
+++ tests/test_autoformat.py
@@ -1,7 +1,8 @@
+import math
 import datetime
 
 import pytest
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 import numpy as np
 
 import autoformat
@@ -14,19 +15,25 @@ class SampleData:
 
         self.data = np.random.random_sample(n)
         time_delta = end - start
-        seconds = time_delta.seconds
-        increment = seconds / n
+        seconds = time_delta.total_seconds()
+        increment = seconds / (n - 1)
         times_list = []
+
         for x in range(0, n):
-            new_time = start + datetime.timedelta(seconds=increment*x)
-            times_list.append(times_list)
+            frac = (math.pi * float(x)) / float(n)
+            self.data[x] = self.data[x]/10.0 + math.cos(frac) 
+
+        for x in range(0, n):
+            new_time = start + datetime.timedelta(seconds=int((increment * x)))
+            times_list.append(new_time)
 
         self.times = np.array(times_list)
 
-    def plot(self):
-
+    def plot(self, filename):
         plt.plot(self.times, self.data)
-        plt.show()
+        plt.savefig(filename)
+        plt.close()
+
 
 def test_same_dates():
     start = datetime.datetime(2015,12,10,12)
@@ -40,13 +47,38 @@ def test_wrong_order():
 
     autoformat.scale(start, end)
 
-def test_3_days():
+def test_time_period(time_span, filename):
     start = datetime.datetime(2015,12,10,12)
-    end = datetime.datetime(2015,12,13,12)
-
+    end = start + time_span
     autoformat.scale(start, end)
     sample = SampleData(start, end, 100)
-    sample.plot()
+    sample.plot(filename)
 
 
-test_same_dates()
+def test_all():
+    test_time_period(datetime.timedelta(seconds=60), "../tests/0_1min.png")
+    test_time_period(datetime.timedelta(seconds=60*30), "../tests/1_30min.png")
+    test_time_period(datetime.timedelta(seconds=60*60), "../tests/2_1hour.png")
+    test_time_period(datetime.timedelta(seconds=60*60*3), "../tests/3_3hour.png")
+    test_time_period(datetime.timedelta(seconds=60*60*6), "../tests/4_6hour.png")
+    test_time_period(datetime.timedelta(seconds=60*60*12), "../tests/5_12hour.png")
+    test_time_period(datetime.timedelta(seconds=60*60*24), "../tests/6_24hour.png")
+    test_time_period(datetime.timedelta(days=3), "../tests/7_3day.png")
+    test_time_period(datetime.timedelta(days=7), "../tests/8_7day.png")
+    test_time_period(datetime.timedelta(days=14), "../tests/9_14day.png")
+    test_time_period(datetime.timedelta(days=30), "../tests/10_30day.png")
+    test_time_period(datetime.timedelta(days=30*3), "../tests/11_3month.png")
+    test_time_period(datetime.timedelta(days=30*6), "../tests/12_6month.png")
+    test_time_period(datetime.timedelta(days=365), "../tests/13_1year.png")
+    test_time_period(datetime.timedelta(days=365*3), "../tests/14_3year.png")
+    test_time_period(datetime.timedelta(days=365*5), "../tests/15_5year.png")
+    test_time_period(datetime.timedelta(days=365*10), "../tests/16_10year.png")
+    test_time_period(datetime.timedelta(days=365*25), "../tests/17_25year.png")
+    test_time_period(datetime.timedelta(days=365*50), "../tests/18_50year.png")
+    test_time_period(datetime.timedelta(days=365*100), "../tests/19_100year.png")
+    test_time_period(datetime.timedelta(days=365*250), "../tests/20_250year.png")
+    test_time_period(datetime.timedelta(days=365*500), "../tests/21_500year.png")
+    test_time_period(datetime.timedelta(days=365*1000), "../tests/22_1000year.png")
+    test_time_period(datetime.timedelta(days=365*3000), "../tests/23_3000year.png")
+
+test_all()
